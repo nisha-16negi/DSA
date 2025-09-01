de3ae996a65f6c72b8dfb28e85c3e934fac81d7d
nisha-16negi <nishanegi.8171@gmail.com>
2025-09-01 19:20:18 +0530

    second largest number in an array

--- findSecondLargestNum.js
+++ findSecondLargestNum.js
@@ -1,17 +1,23 @@
-function findSecondLargestNum() {
+function findSecondLargestNum(arr) {
+  if (arr.length < 2) {
+    // If array has 2 or less than 2 elements
+    return null;
+  }
   let firstLargest = -Infinity;
   let secondLargest = -Infinity;
   for (let i = 0; i < arr.length; i++) {
     if (firstLargest < arr[i]) {
       secondLargest = firstLargest;
       firstLargest = arr[i];
-    } else if (secondLargest < arr[i]) {
+    } 
+    // to check if duplicate element exist
+    else if (secondLargest < arr[i] && arr[i] !== firstLargest) {
       secondLargest = arr[i];
     }
   }
   return secondLargest;
 }
 
-const arr = [20, 2, 11, 5, 3, 17];
+const arr = [20, 2, 17, 5, 3, 20];
 let result = findSecondLargestNum(arr);
 console.log(result);
