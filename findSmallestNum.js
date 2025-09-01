4aff62b2b575300614a91dfb19c04f54db7a6767
nisha-16negi <nishanegi.8171@gmail.com>
2025-09-01 16:35:57 +0530

    Smallest numbers in an array

---
+++ findSmallestNum.js
@@ -0,0 +1,13 @@
+function findSmallestNumber(arr){
+let smallestNum = Infinity;
+ for(let i=0; i<arr.length;i++){
+    if(arr[i]<smallestNum){
+        smallestNum=arr[i];
+    }
+ }
+ return smallestNum;
+}
+
+let arr = [13,8,10,22,11,18];
+let result = findSmallestNumber(arr);
+console.log(result);
